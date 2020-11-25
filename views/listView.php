2895c83ef1385850b3cb1d07499127cb4889b0b4
Ineta Priga <ineta.priga@gmail.com>
2020-11-26 01:05:21 +0200

    stili sakārtoti

--- views/listView.php
+++ views/listView.php
@@ -3,30 +3,26 @@ require_once __DIR__ . "/../components/modifyForm.php";
 
 class listView
 {
-  // private $tasklist;
   private $tasklistUnchecked;
   private $tasklistChecked;
 
-
-  // public function __construct($data = [])
   public function __construct($tasklistUnchecked = [], $tasklistChecked = [])
   {
-    // $this->tasklist = $data;
     $this->tasklistUnchecked = $tasklistUnchecked;
     $this->tasklistChecked = $tasklistChecked;
   }
 
-
   public function html()
-  { ?> <div class="header">
+  { ?>
+    <div class="header">
       <h4>Lietotājs "<?= $_SESSION['username'] ?>" ir pieslēdzies!</h4>
       <form method="POST" class="inline">
         <input name="logOut" type="hidden">
         <button type="submit" class="logout-btn"></button>
       </form>
     </div>
-    <table class="checked-0">
-      <tbody>
+    <table class="checked-no">
+      <tbody id="unchecked">
         <?php
         $last_order_id = sizeof($this->tasklistUnchecked) + sizeof($this->tasklistChecked) + 1;
         foreach ($this->tasklistUnchecked as $key => $task) {
@@ -35,7 +31,10 @@ class listView
         ?>
 
           <tr order_id="<?= $order_id ?>" task="<?= $task['id'] ?>">
-            <td>
+            <td style="width: 40px;">
+              <div class="drag-btn"></div>
+            </td>
+            <td style="width: 40px;">
               <form action="/WB-final-project/?page=check&task_id=<?= $task['id'] ?>&order_id=<?= $ch_order_id ?>&checkbox_value=1" method="post">
                 <input type="checkbox" class="check-btn" name="checked" <?php if ($task["checked"] == '0') {
                                                                           echo "";
@@ -46,7 +45,7 @@ class listView
             </td>
             <td><?= $task["task"] ?></td>
 
-            <td>
+            <td style="width: 80px;">
               <a href="/WB-final-project/?page=list&action=modify&task_id=<?= $task['id'] ?>&order_id=<?= $order_id ?>">
                 <div class="button edit-btn inline"></div>
               </a>
@@ -59,13 +58,14 @@ class listView
 
 
         <?php } ?>
+
       </tbody>
     </table>
+    <div class="center">
+      <a href="/WB-final-project/?page=list&action=modify&last_order_id=<?= $last_order_id ?>" class="add-btn">Pievienot jaunu ierakstu</a></div>
 
-    <a href="/WB-final-project/?page=list&action=modify&last_order_id=<?= $last_order_id ?>">Add task<div class="add-btn inline"></div> </a>
-    <h4>Jau padarīti</h4>
-    <table class="checked-1">
-      <tbody>
+    <table class="checked-yes">
+      <tbody id="checked">
         <?php
         foreach ($this->tasklistChecked as $key => $task) {
           $order_id = $key + sizeof($this->tasklistUnchecked) + 1;
@@ -73,7 +73,7 @@ class listView
         ?>
 
           <tr order_id="<?= $order_id ?>" task="<?= $task['id'] ?>">
-            <td>
+            <td style="width: 40px;">
               <form action="/WB-final-project/?page=check&task_id=<?= $task['id'] ?>&order_id=<?= $ch_order_id ?>&checkbox_value=0" method="post">
                 <input type="checkbox" name="checked" class="check-btn" <?php if ($task["checked"] == '1') {
                                                                           echo "checked";
@@ -84,7 +84,7 @@ class listView
             </td>
             <td><?= $task["task"] ?></td>
 
-            <td>
+            <td style="width: 40px;">
 
               <a href="/WB-final-project/?page=delete&task_id=<?= $task['id'] ?>">
                 <div class="delete-btn-1 inline"></div>
